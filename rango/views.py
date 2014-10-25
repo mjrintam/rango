de98106f309a2c13d129dca92dac8254419b732b
Matti Rintamaki <matti.rintamaki@iki.fi>
2014-10-25 21:26:34 +0300

    Refined HTML links

--- rango/views.py
+++ rango/views.py
@@ -2,9 +2,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Rango says hello world!<br>"
-                        "<a href=\"about\">About Rango</a>")
+                        "<a href='/rango/about'>About Rango</a>")
 
 def about(request):
     return HttpResponse("Rango says: Here is the about page.<br>"
-                        "<a href=\"../rango\">Back to main page</a>")
+                        "<a href='/rango/'>Back to main page</a>")
 
